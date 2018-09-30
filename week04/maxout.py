c3ee5884182022127e0d62c39b299775deae00d7
George Ho <georgeho1618@gmail.com>
2018-09-30 09:57:16 -0400

    asdf

--- week04/maxout.py
+++ week04/maxout.py
@@ -7,11 +7,11 @@ Classify cifar100. Achieve a top-5 accuracy of 70%
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.datasets.cifar10 import load_data
+from tensorflow.keras.datasets.cifar100 import load_data
 from tqdm import tqdm
 
 
-# 2-unit maxout. Adapted from
+# Adapted from
 # https://github.com/philipperemy/tensorflow-maxout/blob/master/maxout.py
 def max_out(inputs):
     shape = inputs.get_shape().as_list()
@@ -27,7 +27,7 @@ np.random.seed(1618)
 tf.set_random_seed(1618)
 
 # Data parameters
-NUM_CLASSES = 10
+NUM_CLASSES = 100
 HEIGHT = 32
 WIDTH = 32
 NUM_CHANNELS = 3
@@ -45,55 +45,74 @@ y_train, y_val, y_test = \
     map(lambda y: tf.keras.utils.to_categorical(y, NUM_CLASSES),
         [y_train, y_val, y_test])
 
+# This means batch size is 40000 / 400 = 100
 x_train_batches = np.split(x_train, 400)
 y_train_batches = np.split(y_train, 400)
 
 # Hyperparameters
 NUM_EPOCHS = 15
-REGULARIZER = tf.keras.regularizers.l2(1e-6)
+REGULARIZER = tf.keras.regularizers.l2(1e-7)
 
 input = tf.placeholder(tf.float32, [None, 32, 32, 3])
 labels = tf.placeholder(tf.float32, [None, NUM_CLASSES])
 
-x = tf.layers.conv2d(input, 64, 3, kernel_regularizer=REGULARIZER)
-x = max_out(x)
-
-x = tf.layers.conv2d(x, 128, 3, kernel_regularizer=REGULARIZER)
+x = tf.layers.conv2d(input, 128, 3, kernel_regularizer=REGULARIZER)
 x = max_out(x)
 
 x = tf.layers.conv2d(x, 256, 3, kernel_regularizer=REGULARIZER)
 x = max_out(x)
 
+x = tf.layers.conv2d(x, 512, 3, kernel_regularizer=REGULARIZER)
+x = max_out(x)
+
+x = tf.layers.conv2d(x, 1024, 3, kernel_regularizer=REGULARIZER)
+x = max_out(x)
+
 x = tf.layers.flatten(x)
-x = tf.layers.dense(x, 128, activation=max_out)
+x = tf.layers.dense(x, 256, activation=max_out)
 logits = tf.layers.dense(x, NUM_CLASSES)
 output = tf.nn.softmax(logits)
 
-correct_prediction = tf.equal(tf.argmax(output, 1), tf.argmax(labels, 1))
-accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+# Metrics
+accuracy = tf.keras.metrics.categorical_accuracy(labels, output)
+top5_accuracy = tf.keras.metrics.top_k_categorical_accuracy(labels,
+                                                            output, k=5)
 
-top5_accuracy = tf.keras.metrics.top_k_categorical_accuracy(labels, output, k=5)
-
+# Loss
 loss = tf.losses.softmax_cross_entropy(labels, logits)
 train_step = tf.train.AdamOptimizer().minimize(loss)
 
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
+LOGFILE = '100metrics.txt'
 saver = tf.train.Saver()
 
 for i in range(NUM_EPOCHS):
-    for x_batch, y_batch in tqdm(zip(x_train_batches, y_train_batches)):
-        sess.run(train_step, feed_dict={input: x_batch, labels: y_batch})
+    for j, x_batch, y_batch in tqdm(enumerate(zip(x_train_batches,
+                                                  y_train_batches))):
+        if j % 40 == 0:
+            _, accuracy_, top5_accuracy = \
+                sess.run([train_step, accuracy, top5_accuracy],
+                         feed_dict={input: x_batch, labels: y_batch})
+            with open(LOGFILE, 'a') as f:
+                f.write('Minibatch: {}\t'.format(j))
+                f.write('Train Loss: {}\t'.format(loss_))
+                f.write('Train Accuracy: {}\t'.format(accuracy_))
+                f.write('Train Top5 Accuracy: {}\n'.format(top5_accuracy_))
+        else:
+            sess.run(train_step, feed_dict={input: x_batch, labels: y_batch})
 
     loss_, accuracy_, top5_accuracy_ = \
         sess.run([loss, accuracy, top5_accuracy],
                  feed_dict={input: x_val, labels: y_val})
 
-    with open('metrics.txt', 'a') as f:
+    with open(LOGFILE, 'a') as f:
         f.write('Epoch: {}\t'.format(i))
         f.write('Loss: {}\t'.format(loss_))
         f.write('Accuracy: {}\t'.format(accuracy_))
         f.write('Top5 Accuracy: {}\n'.format(top5_accuracy_))
+        f.write(80*'=')
+        f.write('\n')
 
-    save_path = saver.save(sess, "./tmp/model{}.ckpt".format(i))
+    save_path = saver.save(sess, "./tmp/100model{}.ckpt".format(i))
